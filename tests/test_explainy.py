ffa190ac082a7f822c01872ddd6db1eb3fa3901f
Mauro Luzzatto <mauroluzzatto@hotmail.com>
2021-07-27 10:56:23 +0200

    update dummy test

--- tests/test_explainy.py
+++ tests/test_explainy.py
@@ -35,3 +35,7 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+
+
+def test_setup():
+    assert True == 1
