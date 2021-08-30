98410eeafe1207e417a1e30b420c7a45461ff158
Mauro Luzzatto <mauroluzzatto@hotmail.com>
2021-08-30 22:59:59 +0200

    update docs

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('HISTORY.rst') as history_file:
 
 # requirements = ['Click>=7.0', 'scikit-learn>=0.24.1', 'pandas', 'matplotlib'] #read_requirements(os.path.join('requirements', 'requirements.txt'))
 requirements = ['Click', 'scikit-learn', 'pandas', 'matplotlib'] 
-(?# docs_extras = read_requirements(r'requirements_dev.txt'))
+# docs_extras = read_requirements(r'requirements_dev.txt'))
 test_requirements = ['pytest>=3', ]
 
 setup(
