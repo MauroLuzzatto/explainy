6dfe009a2ee10fbd66729a0aeab17e9d1e5da365
Mauro Luzzatto <mauroluzzatto@hotmail.com>
2021-09-16 23:18:47 +0200

    Bump version: 0.1.7 → 0.1.8

--- explainy/__init__.py
+++ explainy/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Mauro Luzzatto"""
 __email__ = 'mauroluzzatto@hotmail.com'
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 
 
 from explainy.explanation.permutation_explanation import PermutationExplanation
