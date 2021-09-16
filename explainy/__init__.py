dd4e22f2b2369eff40b0acc1978cfa7a87394e25
Mauro Luzzatto <mauroluzzatto@hotmail.com>
2021-09-16 23:18:39 +0200

    update init

--- explainy/__init__.py
+++ explainy/__init__.py
@@ -1,8 +1,8 @@
 """Top-level package for explainy."""
 
 __author__ = """Mauro Luzzatto"""
-__email__ = "mauroluzzatto@hotmail.com"
-__version__ = "0.1.7"
+__email__ = 'mauroluzzatto@hotmail.com'
+__version__ = '0.1.7'
 
 
 from explainy.explanation.permutation_explanation import PermutationExplanation
