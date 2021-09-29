63b53654fbc31cc87eba8d8d4dba15e779a72853
Mauro Luzzatto <mauroluzzatto@hotmail.com>
2021-09-29 22:49:35 +0200

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -19,10 +19,10 @@
 #
 import os
 import sys
-import explainy
 
 sys.path.insert(0, os.path.abspath('..'))
 
+import explainy
 
 
 # -- General configuration ---------------------------------------------
