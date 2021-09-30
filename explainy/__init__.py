6c6824c2b80152c91352f470beaf6e5a6a1d2d32
Mauro Luzzatto <mauroluzzatto@hotmail.com>
2021-09-30 22:09:16 +0200

    Bump version: 0.1.9 → 0.1.10

--- explainy/__init__.py
+++ explainy/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Mauro Luzzatto"""
 __email__ = "mauroluzzatto@hotmail.com"
-__version__ = '0.1.9'
+__version__ = '0.1.10'
--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MauroLuzzatto/explainy',
-    version='0.1.9',
+    version='0.1.10',
     zip_safe=False,
 )
