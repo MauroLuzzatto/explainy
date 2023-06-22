5d3f3d5d8fcda5de11a40d3b9d68a0f23308940e
Mauro Luzzatto <mauroluzzatto@hotmail.com>
2023-06-22 23:37:58 +0200

    update plotting

--- explainy/explanations/counterfactual_explanation.py
+++ explainy/explanations/counterfactual_explanation.py
@@ -367,7 +367,7 @@ class CounterfactualExplanation(ExplanationBase):
 
         # if show_rating:
         # make the last row bold
-        for (row, col), cell in table.get_celld().items():
+        for (row, _), cell in table.get_celld().items():
             if row == array_subset.shape[0]:
                 cell.set_text_props(fontproperties=FontProperties(weight="bold"))
 
@@ -375,14 +375,14 @@ class CounterfactualExplanation(ExplanationBase):
         plt.grid("off")
         # draw canvas once
         plt.gcf().canvas.draw()
-        # get bounding box of table
-        points = table.get_window_extent(plt.gcf()._cachedRenderer).get_points()
-        # add 10 pixel spacing
-        points[0, :] -= 10
-        points[1, :] += 10
+        # # get bounding box of table
+        # points = table.get_window_extent(plt.gcf()._cachedRenderer).get_points()
+        # # add 10 pixel spacing
+        # points[0, :] -= 10
+        # points[1, :] += 10
 
-        # get new bounding box in inches
-        self.nbbox = matplotlib.transforms.Bbox.from_extents(points / plt.gcf().dpi)
+        # # get new bounding box in inches
+        # self.nbbox = matplotlib.transforms.Bbox.from_extents(points / plt.gcf().dpi)
         plt.show()
         return fig
 
