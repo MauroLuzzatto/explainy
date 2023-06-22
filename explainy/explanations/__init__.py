3f87babf1d3ef9e11d1aacaa67bc535dd8c87e40
Mauro Luzzatto <mauroluzzatto@hotmail.com>
2023-06-22 23:10:22 +0200

    clean repo

---
+++ explainy/explanations/__init__.py
@@ -0,0 +1,4 @@
+from explainy.explanations.counterfactual_explanation import CounterfactualExplanation
+from explainy.explanations.permutation_explanation import PermutationExplanation
+from explainy.explanations.shap_explanation import ShapExplanation
+from explainy.explanations.surrogate_model_explanation import SurrogateModelExplanation
