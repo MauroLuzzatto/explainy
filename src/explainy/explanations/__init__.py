31851ce235e25b3450a9e585e17ce821dd4b380f
Mauro Luzzatto <mauroluzzatto@hotmail.com>
2024-01-13 21:58:34 +0100

    update init file

--- src/explainy/explanations/__init__.py
+++ src/explainy/explanations/__init__.py
@@ -1,1 +1,10 @@
+from explainy.explanations.counterfactual_explanation import (
+    CounterfactualExplanation,
+)  # noqa: F401
+from explainy.explanations.permutation_explanation import (
+    PermutationExplanation,
+)  # noqa: F401
 from explainy.explanations.shap_explanation import ShapExplanation  # noqa: F401
+from explainy.explanations.surrogate_model_explanation import (
+    SurrogateModelExplanation,
+)  # noqa: F401
