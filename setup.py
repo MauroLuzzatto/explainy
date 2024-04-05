8ff07239eadde82b299a9083e19c0ca963c515dc
Mauro Luzzatto <mauroluzzatto@hotmail.com>
2024-04-05 12:56:22 +0200

    Bump version: 0.2.8 → 0.2.9

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/MauroLuzzatto/explainy",
-    version='0.2.8',
+    version='0.2.9',
     zip_safe=False,
 )
 # fmt: on
